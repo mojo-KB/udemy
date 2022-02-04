4d8314f213d24085951b979a14c31a390dce7d53
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-02-04 18:54:42 -0500

    filter method

--- filter.js
+++ filter.js
@@ -3,3 +3,12 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 const even = numbers.filter(n => n % 2 == 0);
 
 console.log(even);
+
+
+
+//
+
+function validUserNames(arr) {
+    const newarr = arr.filter(i => i.length < 10);
+    return newarr;
+}
