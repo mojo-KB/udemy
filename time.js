7a855b6c587dedf1ed523e99592d6eea374d283b
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-02-04 18:23:35 -0500

    Section 22 Callbacks and Array Methods

---
+++ time.js
@@ -0,0 +1,11 @@
+// setTimeout and setInterval
+
+setTimeout(() => {
+    console.log("... Are you still there?");
+}, 3000)
+
+// 3000ms = 3s
+
+const id = setInterval9(() => {
+    console.log("Math.random()");
+}, 2000)
