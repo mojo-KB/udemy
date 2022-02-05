838ea400803e65d321e38ca887d85eed1eb48ae5
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-02-04 19:03:48 -0500

    arrow function

--- some.js
+++ some.js
@@ -3,3 +3,9 @@ const n = [1, 2, 3, 4, 5, 6, 7]
 
 hasSomeEven = n.some(i => i % 2 == 0)
 allEven = n.every(i => i % 2 == 0)
+
+
+//function 
+const allEvens = (arr) => {
+    return arr.every(i => i % 2 === 0);
+}
