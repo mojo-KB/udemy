aeccd296bf4c47b62f93a7dd0dbd47482ffba0ae
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-02-10 01:03:27 -0500

    rbg color app

--- time.js
+++ time.js
@@ -9,3 +9,17 @@ setTimeout(() => {
 const id = setInterval9(() => {
     console.log("Math.random()");
 }, 2000)
+
+document.getElementById("container").style.textAlign = 'center';
+
+document.querySelector('img').style.width = '150px';
+document.querySelector('img').style.borderRadius = '50%';
+document.querySelector('img').style.textColor
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOU CODE GOES HERE:
+let len = document.getElementsByTagName('span').length;
+for (let i = 0; i < len; i++) {
+    document.querySelector('#span:nth')
+}
