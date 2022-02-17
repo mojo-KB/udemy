50ce5ddeb34202732df45b18a194c2bd84825fd5
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-02-17 12:53:36 -0500

    score keeper part1

--- score_keeper/app.js
+++ score_keeper/app.js
@@ -1,6 +1,32 @@
-const h1 = document.querySelector('h1');
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const player1 = document.querySelector('#player1');
+const player2 = document.querySelector('#player2')
 
-h1.addEventListener('click', (e) => {
-    e.preventDefault();
-    alert('Hello, world');
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let isGameOver = false;
+
+
+
+p1Button.addEventListener('click', function() {
+    if (!isGameOver) {
+        p1Score += 1;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+        }
+        player1.textContent = p1Score;
+    }
+})
+
+
+p2Button.addEventListener('click', () => {
+    if (!isGameOver) {
+        p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+        }
+        player2.textContent = p2Score;
+    }
 })
