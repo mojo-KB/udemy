22d12587a67a01ed5eb5b73477affe85c75eafb9
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-02-17 14:30:01 -0500

    score keeper part2

--- score_keeper/app.js
+++ score_keeper/app.js
@@ -1,11 +1,13 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const player1 = document.querySelector('#player1');
-const player2 = document.querySelector('#player2')
+const player2 = document.querySelector('#player2');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 
@@ -13,7 +15,9 @@ let isGameOver = false;
 p1Button.addEventListener('click', function() {
     if (!isGameOver) {
         p1Score += 1;
-        if (p1Score === winningScore) {
+        if (p1Score - winningScore === 0) {
+            player1.classList.add('winner');
+            player2.classList.add('loser');
             isGameOver = true;
         }
         player1.textContent = p1Score;
@@ -24,9 +28,31 @@ p1Button.addEventListener('click', function() {
 p2Button.addEventListener('click', () => {
     if (!isGameOver) {
         p2Score += 1;
-        if (p2Score === winningScore) {
+        if (p2Score - winningScore === 0) {
+            player2.classList.add('winner');
+            player1.classList.add('loser');
             isGameOver = true;
         }
         player2.textContent = p2Score;
     }
 })
+
+
+resetButton.addEventListener('click', reset);
+
+
+winningScoreSelect.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    player1.textContent = p1Score;
+    player2.textContent = p2Score;
+    player1.classList.remove('winner', 'loser');
+    player2.classList.remove('winner', 'loser');
+}
