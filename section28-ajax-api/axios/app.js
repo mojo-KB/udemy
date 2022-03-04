09806a49b62c07b276f62a4efc217291e53047fc
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-03-03 19:10:42 -0500

    axios with try and catch

--- section28-ajax-api/axios/app.js
+++ section28-ajax-api/axios/app.js
@@ -9,3 +9,23 @@ axios({
     .then(function(response) {
         console.log(response);
     });
+
+console.log("Hello")
+const response = async() => {
+    try {
+        let data = await axios({
+            method: 'GET',
+            url: 'https://icanhazdadjoke.com/2',
+            headers: {
+                'Accept': 'Application/Json'
+            }
+            //responseType: 'stream'
+        })
+        console.log(data);
+    } catch (e) {
+        console.log("Error", e);
+    }
+
+}
+
+response();
