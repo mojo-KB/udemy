0b88c765c8bbfba825ba968ae931f959b3aa06dd
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-03-16 00:06:34 -0400

    file structure of CRUD

--- section35_RESTful/postRoutes/index.js
+++ section35_RESTful/postRoutes/index.js
@@ -12,3 +12,10 @@ app.post('/tacos', (req, res) => {
 app.listen(8000, () => {
     console.log("Listening on port 8000");
 })
+
+
+
+// File Structure of CRUD
+
+// GET /comments - list all comments
+// POST /comments - create a new comment
