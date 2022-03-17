73fe5b6f5170dff424d718ab24e0b0d1205bc425
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-03-17 01:20:09 -0400

    restful comment

--- section35_RESTful/postRoutes/index.js
+++ section35_RESTful/postRoutes/index.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+
+
+const comments = [
+    {
+        username: "Bunla",
+        comment: "Lol that is so funny"
+    },
+    {
+        username: "Skyler",
+        comment: "I like to go birdwatching with my dog"
+    },
+    {
+        username: "Sk8erBoi",
+        comment: "Please detete yoru account, Todd"
+    },
+    {
+        username: "onlysayswolf",
+        comment: "woof woof woof"
+    }
+
+]
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
+})
 
 app.get('/tacos', (req, res) => {
     res.send("Get /tacos response");
@@ -16,6 +48,10 @@ app.listen(8000, () => {
 
 
 // File Structure of CRUD
+// with http verbs
 
 // GET /comments - list all comments
 // POST /comments - create a new comment
+// GET /comments/:id - get one comment using ID
+// PATCH /comments/:id - update one comment
+// DELETE /comments/:id - delete one comment
